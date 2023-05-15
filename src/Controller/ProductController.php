c5c17f988201f82b42ac110fe394c9a0a94c5072
Jeremy Duc <duc.jeremy@sfr.fr>
2023-05-15 17:00:50 +0200

    PUSH 15/05 end

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,6 +26,38 @@ class ProductController extends AbstractController
     {
         return $this->render('product/show.html.twig', [
             'product' => $product,
+        ]);
+    }
+
+    #[Route('/admin/product/create', name: 'app_product_create', priority: 2)]
+    #[Route('/admin/product/edit/{id}', name: 'app_product_edit', priority: 2)]
+    public function create(Request $request, EntityManagerInterface $manager, Product $product=null): Response
+    {
+        $edit = false;
+        if ($product){
+            $edit = true;
+        }
+        if (!$edit){
+            $product = new Product();
+        }
+        $productForm = $this->createForm(ProductType::class, $product);
+        $productForm->handleRequest($request);
+        if ($productForm->isSubmitted() && $productForm->isValid())
+        {
+            if (!$edit){
+                $images = $productForm->getData()->getImages();
+                foreach($images as $image){
+                    $image->setGateau($product);
+                }
+            }
+            $manager->persist($product);
+            $manager->flush();
+            return $this->redirectToRoute('app_product_show', ['id'=>$product->getId()]);
+        }
+
+        return $this->renderForm('product/create.html.twig', [
+            'productForm'=>$productForm,
+            'edit'=>$edit
         ]);
     }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -22,6 +24,14 @@ class Product
 
     #[ORM\Column]
     private ?int $price = null;
+
+    #[ORM\OneToMany(mappedBy: 'product', targetEntity: Image::class, cascade: ['persist'], orphanRemoval: true)]
+    private Collection $images;
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -60,6 +70,36 @@ class Product
     public function setPrice(int $price): self
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Image>
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images->add($image);
+            $image->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getProduct() === $this) {
+                $image->setProduct(null);
+            }
+        }
 
         return $this;
     }
