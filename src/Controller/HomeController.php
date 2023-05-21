18d9d35ddcdf0bcb8938e69ae57cacbbf78171f9
jeremy <nashoba.jeremy@gmail.com>
2023-05-21 19:04:07 +0200

    end sunday

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,17 +6,21 @@ use App\Entity\Category;
 use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Knp\Snappy\Pdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
+
+
 }
--- src/Controller/MyOrdersController.php
+++ src/Controller/MyOrdersController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Order;
+use Knp\Snappy\Pdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
 #[Route('/my/orders')]
 class MyOrdersController extends AbstractController
 {
@@ -28,5 +33,22 @@ class MyOrdersController extends AbstractController
         ]);
     }
 
+    #[Route('/mailinvoice/{id}', name: 'app_mail_invoice')]
+    public function mailInvoice(MailerInterface $mailer, Pdf $pdfmaker, Environment $twig, Order $order)
+    {
+        $html = $twig->render('home/invoice.html.twig',[
+            'order'=>$order
+        ]);
+        $pdf = $pdfmaker->getOutputFromHtml($html);
 
+        $email = (new Email())
+            ->from()
+            ->to('nashoba.jeremy@gmail.com')
+            ->subject('Votre facture')
+            ->text('Voici votre facture pour votre dernière commande !')
+            ->attach($pdf, sprintf('facture.pdf'));
+
+        $mailer->send($email);
+        return $this->redirectToRoute('app_my_orders_show', ['id'=>$order->getId()]);
+    }
 }
